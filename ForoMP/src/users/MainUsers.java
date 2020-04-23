bf05529dbf815afa657b9d7352d86631904c1b02
Sara González Terroba <Sara@LAPTOP-ONMG78EK>
2020-04-23 21:21:12 +0200

    Únicamente he modificado el main con el que estoy haciendo pruebas

--- ForoMP/src/users/MainUsers.java
+++ ForoMP/src/users/MainUsers.java
@@ -5,6 +5,10 @@
  */
 package users;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,66 +17,13 @@ import java.util.Scanner;
  * @author Sara
  */
 public class MainUsers {
-    //public ArrayList<Usuario> usuarios = new ArrayList<>();
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        /*System.out.println("Qué usuario quieres crear? Las posibilidades son: Alumno, Profesor o Administrador");
-        Scanner scan = new Scanner(System.in);
-        String peticion = scan.nextLine();
-        if (peticion.equalsIgnoreCase("Alumno")) {
-           System.out.println("Cuál es el nombre?");
-           String nombre = scan.nextLine();
-           System.out.println("Cuál es el primer apellido?");
-           String apellido1 = scan.nextLine();
-           System.out.println("Cuál es el segundo apellido?");
-           String apellido2 = scan.nextLine();
-           System.out.println("Cuál es el nick?");
-           String nick = scan.nextLine();
-           System.out.println("Cuál es el email?");
-           String email = scan.nextLine();
-           System.out.println("Establece la contraseña");
-           String contraseña = scan.nextLine();
-           Alumno user = new Alumno(nombre, apellido1, apellido2, nick, email, contraseña);
-           System.out.print(user.getNombre());
-           user.penalizar();
-           if (user.penalizado()) System.out.println("está penliazado");
-           else System.out.println("NO está penliazado");}
+    private ArrayList<Usuario> usuarios = new ArrayList<>();
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         
-           else if (peticion.equalsIgnoreCase("Profesor")){
-           System.out.println("Cuál es el nombre?");
-           String nombre = scan.nextLine();
-           System.out.println("Cuál es el primer apellido?");
-           String apellido1 = scan.nextLine();
-           System.out.println("Cuál es el segundo apellido?");
-           String apellido2 = scan.nextLine();
-           System.out.println("Cuál es el nick?");
-           String nick = scan.nextLine();
-           System.out.println("Cuál es el email?");
-           String email = scan.nextLine();
-           System.out.println("Establece la contraseña");
-           String contraseña = scan.nextLine();
-           Profesor user = new Profesor(nombre, apellido1, apellido2, nick, email, contraseña);
-           System.out.print(user.getNombre());} 
-           
-           else {System.out.println("Cuál es el nombre?");
-           String nombre = scan.nextLine();
-           System.out.println("Cuál es el primer apellido?");
-           String apellido1 = scan.nextLine();
-           System.out.println("Cuál es el segundo apellido?");
-           String apellido2 = scan.nextLine();
-           System.out.println("Cuál es el nick?");
-           String nick = scan.nextLine();
-           System.out.println("Cuál es el email?");
-           String email = scan.nextLine();
-           System.out.println("Establece la contraseña");
-           String contraseña = scan.nextLine();
-           Administrador user = new Administrador(nombre, apellido1, apellido2, nick, email, contraseña);
-           System.out.print(user.getNombre());}
-           
-        
-        */
     }
     
 }
